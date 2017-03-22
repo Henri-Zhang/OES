d0d33bc9554174ae28c8d922b39abf975fb770c9
LikedBlack <371595867@qq.com>
2017-03-22 22:49:11 +0800

    2017-03-22 22:49:11

--- oes-workspcae/modules/oes-portlet/src/main/java/priv/barrow/oes/portlet/signin/controller/SignInPortlet.java
+++ oes-workspcae/modules/oes-portlet/src/main/java/priv/barrow/oes/portlet/signin/controller/SignInPortlet.java
@@ -1,10 +1,20 @@
 package priv.barrow.oes.portlet.signin.controller;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.ProcessAction;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
 
 /**
  * @author barrow
@@ -26,5 +36,25 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 )
 
 public class SignInPortlet extends MVCPortlet {
+
+    @Override
+    public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
+            throws IOException, PortletException {
+        ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+        boolean isSignedIn = themeDisplay.isSignedIn();
+        if (isSignedIn) {
+            String fullName = themeDisplay.getUser().getFullName();
+            renderRequest.setAttribute("fullName", fullName);
+        }
+
+        renderRequest.setAttribute("isSignedIn", isSignedIn);
+        super.doView(renderRequest, renderResponse);
+    }
+
+    @ProcessAction(name = "login")
+    public void login(ActionRequest actionRequest, ActionResponse actionResponse) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 
 }
